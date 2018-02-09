4ceb57d4fd7a3734e5189253774a6790c6d4e150
vivian <tianwenwen555@sina.com>
2018-02-09 17:39:36 +0800

    zegnjai

--- server.js
+++ server.js
@@ -68,7 +68,45 @@ const httpServer = http.createServer(function(req, res) {
         res.writeHeader(200, {
             'Content-Type': 'application/json;charset=utf-8'
         });
+         let {
+            user,
+            pass
+        } = query;
+         if(!pass || !user){
+            res.write('密码或密码不能为空。。');
+            res.end();
+        }else{
+             pool.getConnection((err, db) => {
 
+                db.query(`SELECT username FROM user_table where username='${user}'`,(error, results, fields) => {
+                    if (error){
+                        res.write(JSON.stringify({code:1,msg:'数据库出错'}));
+                        res.end();
+                    }else if (results && results.length) {
+                        db.query(`SELECT username FROM user_table where username='${user}' and password='${pass}'`, (error, results, fields) => {
+                             if (error){
+                                res.write(JSON.stringify({code:1,msg:'数据库出错'}));
+                                res.end();
+                             }else if(results && results.length){
+                                 console.log(221)
+                                res.write(JSON.stringify({code:2,msg:'登陆成功'}));
+                                res.end();
+                             }else{
+                                 console.log(222)
+                                res.write(JSON.stringify({code: 1,msg: "用户名或密码错误" }));
+                                res.end();
+                             }
+                        })
+                    }else{
+                        res.write(JSON.stringify({code: 1,msg: "此用户不存在" }));
+                        res.end();
+                    }
+
+                })
+             })
+
+        }
+       
 
     } else {
         //请求文件
